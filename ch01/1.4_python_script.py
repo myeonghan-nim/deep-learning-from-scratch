9e928e23be42c95ab0d2a203d652605db401617e
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-09 23:01:24 +0900

    study python with script

--- ch01/1.4_python_script.py
+++ ch01/1.4_python_script.py
@@ -1,7 +1,18 @@
-# coding: utf-8
-class Man:
-    '''sample class'''
+# study python with using script file
 
+# 1. save files
+
+'''
+for run file with terminal(powershell, bash, etc)
+enter python your_file_name.py in terminal
+'''
+
+print('I am hungry!')
+
+# 2. class
+
+
+class Man:
     def __init__(self, name):
         self.name = name
         print('Initilized!')
@@ -11,6 +22,7 @@ class Man:
 
     def goodbye(self):
         print('Good-bye ' + self.name + '!')
+
 
 m = Man('David')
 m.hello()
