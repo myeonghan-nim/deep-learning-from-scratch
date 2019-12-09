a2d493c16ef678471aba0be51ec32bfab9e230b6
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-09 23:01:36 +0900

    study numpy in python

---
+++ ch01/1.5_python_numpy.py
@@ -0,0 +1,42 @@
+# study numpy
+# 1. outer library, Numpy
+import numpy as np  # import numpy for using library
+
+# 2. 1st array, list
+
+x = np.array([1.0, 2.0, 3.0])
+print(x)
+print(type(x))
+
+# 3. calculation
+
+y = np.array([2.0, 4.0, 6.0])
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+print(x / 2)  # broadcast
+
+# 4. Nth array
+
+A = np.array([[1, 2], [3, 4]])
+print(A)
+print(A.shape)
+print(A.dtype)
+
+B = np.array([[3, 0], [0, 6]])
+print(A + B)
+print(A * B)
+
+print(A)
+print(A * 10)  # broadcast
+
+# 5. Broadcast
+'''
+when using numpy array
+if size of A mat and B mat is diff
+numpy match size of mat with broadcast
+'''
+C = np.array([10, 20])
+print(A * C)
