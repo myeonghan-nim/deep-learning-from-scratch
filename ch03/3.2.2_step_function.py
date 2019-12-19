dad5f88a8a4e229f54fcfc866e58928bf77898d6
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-19 22:16:51 +0900

    rename & modify files

--- ch03/3.2.2_step_function.py
+++ ch03/3.2.2_step_function.py
@@ -1,13 +1,16 @@
-# coding: utf-8
 import numpy as np
 import matplotlib.pylab as plt
 
 
 def step_function(x):
+    # dtype changes True/False to 1/0 so it returns np.array with 1/0
     return np.array(x > 0, dtype=np.int)
 
+
+# check each atom in X is over 0
 X = np.arange(-5.0, 5.0, 0.1)
 Y = step_function(X)
+
 plt.plot(X, Y)
-plt.ylim(-0.1, 1.1)  # y축의 범위 지정
+plt.ylim(-0.1, 1.1)
 plt.show()
--- ch03/3.2.7_relu.py
+++ ch03/3.2.7_relu.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 import numpy as np
 import matplotlib.pylab as plt
 
@@ -6,8 +5,10 @@ import matplotlib.pylab as plt
 def relu(x):
     return np.maximum(0, x)
 
+
 x = np.arange(-5.0, 5.0, 0.1)
 y = relu(x)
+
 plt.plot(x, y)
 plt.ylim(-1.0, 5.5)
 plt.show()
