1f9477c85fd376d197cfd4af4918401a33d23479
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-31 18:47:03 +0900

    study 3.6.2_neural_network_with_mnist.py

---
+++ ch03/3.6.2_neural_network_with_mnist.py
@@ -0,0 +1,53 @@
+# 0. old import method
+# import sys
+# import os
+
+# sys.path.append(os.pardir)
+
+# from common.functions import sigmoid, softmax
+# from dataset.mnist import load_mnist
+
+# 1. new import method
+from functions import sigmoid, softmax
+from mnist import load_mnist
+import pickle
+import numpy as np
+
+
+def get_data():  # def function for load mnist datasets with normalization
+    (x_train, t_train), (x_test, t_test) = load_mnist(
+        normalize=True, flatten=True, one_hot_label=False
+    )
+    return x_test, t_test
+
+
+def init_network():  # def function for read wieght parameters
+    with open('ch03/3.x_weight.pkl', 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)   # return index of max probability
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print('Accuracy:' + str(float(accuracy_cnt) / len(x)))
