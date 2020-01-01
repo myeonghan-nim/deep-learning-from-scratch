7dcba966722efb599648d3b62b40a52512a09004
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-02 01:44:20 +0900

    study 4.4.2_gradient_in_neural.py

---
+++ ch04/4.4.2_gradient_in_neural.py
@@ -0,0 +1,31 @@
+import numpy as np
+from gradient import numerical_gradient
+from functions import softmax, cross_entropy_error
+
+
+class simpleNet:
+    def __init__(self):  # init values with one-hot-encoind
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):  # calculate error
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+
+x = np.array([0.6, 0.9])
+t = np.array([0, 0, 1])
+
+net = simpleNet()
+
+
+def f(w):
+    return net.loss(x, t)
+
+
+dW = numerical_gradient(f, net.W)
+print(dW)
