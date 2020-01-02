5514004afb3892098898216af1a566811fefcd62
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-02 20:46:10 +0900

    study 5.7.3_backpropagation_gradient_check.py

---
+++ ch05/5.7.3_backpropagation_gradient_check.py
@@ -0,0 +1,21 @@
+import numpy as np
+from mnist import load_mnist
+from neural_network import TwoLayerNet
+
+# 데이터 읽기
+(x_train, t_train), (x_test, t_test) = load_mnist(
+    normalize=True, one_hot_label=True
+)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+# 각 가중치의 절대 오차의 평균을 구한다.
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+    print(key + ':' + str(diff))
