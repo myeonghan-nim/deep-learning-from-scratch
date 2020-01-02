67f9ea749a6f7a51f9d48072f70b7713c7baef12
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-02 20:45:11 +0900

    modify files with autopep8

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -1,22 +1,16 @@
-# coding: utf-8
-
-
 class MulLayer:
     def __init__(self):
         self.x = None
         self.y = None
 
-    def forward(self, x, y):
+    def forward(self, x, y):  # 순전파
         self.x = x
-        self.y = y                
-        out = x * y
+        self.y = y
+        return x * y
 
-        return out
-
-    def backward(self, dout):
-        dx = dout * self.y  # x와 y를 바꾼다.
+    def backward(self, dout):  # 역전파
+        dx = dout * self.y  # 역전파는 순전파를 바꾸어 전달합니다.
         dy = dout * self.x
-
         return dx, dy
 
 
@@ -25,12 +19,9 @@ class AddLayer:
         pass
 
     def forward(self, x, y):
-        out = x + y
-
-        return out
+        return x + y
 
     def backward(self, dout):
         dx = dout * 1
         dy = dout * 1
-
         return dx, dy
