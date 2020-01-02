01c6cf5d7cce33d60fed54d9d2057d88f048e555
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-02 20:44:11 +0900

    study 5.4.1_multiple_layer.py

--- ch05/5.4.1_multiple_layer.py
+++ ch05/5.4.1_multiple_layer.py
@@ -1,6 +1,4 @@
-# coding: utf-8
 from layer_naive import *
-
 
 apple = 100
 apple_num = 2
@@ -13,12 +11,13 @@ mul_tax_layer = MulLayer()
 apple_price = mul_apple_layer.forward(apple, apple_num)
 price = mul_tax_layer.forward(apple_price, tax)
 
+print('price:', int(price))
+
 # backward
 dprice = 1
 dapple_price, dtax = mul_tax_layer.backward(dprice)
 dapple, dapple_num = mul_apple_layer.backward(dapple_price)
 
-print("price:", int(price))
-print("dApple:", dapple)
-print("dApple_num:", int(dapple_num))
-print("dTax:", dtax)
+print('dApple:', dapple)
+print('dApple_num:', int(dapple_num))
+print('dTax:', dtax)
