ea3a75415a21972690eb617b25c49ff5e06193a0
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-03 21:11:25 +0900

    study 6.2.2_weight_init_activation_histogram.py

--- ch06/6.2.2_weight_init_activation_histogram.py
+++ ch06/6.2.2_weight_init_activation_histogram.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,11 +12,12 @@ def ReLU(x):
 
 def tanh(x):
     return np.tanh(x)
-    
+
+
 input_data = np.random.randn(1000, 100)  # 1000개의 데이터
 node_num = 100  # 각 은닉층의 노드(뉴런) 수
-hidden_layer_size = 5  # 은닉층이 5개
-activations = {}  # 이곳에 활성화 결과를 저장
+hidden_layer_size = 5  # 은닉층 5개
+activations = {}  # 활성화 결과 저장
 
 x = input_data
 
@@ -31,9 +31,7 @@ for i in range(hidden_layer_size):
     # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
     # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num)
 
-
     a = np.dot(x, w)
-
 
     # 활성화 함수도 바꿔가며 실험해보자！
     z = sigmoid(a)
@@ -45,9 +43,13 @@ for i in range(hidden_layer_size):
 # 히스토그램 그리기
 for i, a in activations.items():
     plt.subplot(1, len(activations), i+1)
-    plt.title(str(i+1) + "-layer")
-    if i != 0: plt.yticks([], [])
-    # plt.xlim(0.1, 1)
-    # plt.ylim(0, 7000)
-    plt.hist(a.flatten(), 30, range=(0,1))
+    plt.title(str(i + 1) + '-layer')
+
+    if i != 0:
+        plt.yticks([], [])
+
+    plt.xlim(0.1, 1)
+    plt.ylim(0, 7000)
+    plt.hist(a.flatten(), 30, range=(0, 1))
+
 plt.show()
