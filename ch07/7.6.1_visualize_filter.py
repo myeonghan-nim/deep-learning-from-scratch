76612b2da13a4288dae49030d625caab0ed42914
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-17 21:49:59 +0900

    rename files for chapter07

--- ch07/7.6.1_visualize_filter.py
+++ ch07/7.6.1_visualize_filter.py
@@ -1,28 +1,28 @@
-# coding: utf-8
-import numpy as np
+from cnn import SimpleConvNet
 import matplotlib.pyplot as plt
-from simple_convnet import SimpleConvNet
+import numpy as np
+
 
 def filter_show(filters, nx=8, margin=3, scale=10):
-    """
+    '''
     c.f. https://gist.github.com/aidiary/07d530d5e08011832b12#file-draw_weight-py
-    """
+    '''
     FN, C, FH, FW = filters.shape
     ny = int(np.ceil(FN / nx))
 
     fig = plt.figure()
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+    fig.subplots_adjust(left=0, right=1, bottom=0,
+                        top=1, hspace=0.05, wspace=0.05)
 
     for i in range(FN):
-        ax = fig.add_subplot(ny, nx, i+1, xticks=[], yticks=[])
+        ax = fig.add_subplot(ny, nx, i + 1, xticks=[], yticks=[])
         ax.imshow(filters[i, 0], cmap=plt.cm.gray_r, interpolation='nearest')
+
     plt.show()
 
 
-network = SimpleConvNet()
-# 무작위(랜덤) 초기화 후의 가중치
+network = SimpleConvNet()  # 랜덤 초기화 후 가중치
 filter_show(network.params['W1'])
 
-# 학습된 가중치
-network.load_params("params.pkl")
+network.load_params('ch07/params.pkl')  # 학습된 가중치
 filter_show(network.params['W1'])
