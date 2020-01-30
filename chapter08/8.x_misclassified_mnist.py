5602add9fd8af9ab20078b105f821774eb42c118
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-01-30 21:25:28 +0900

    rename files

--- chapter08/8.x_misclassified_mnist.py
+++ chapter08/8.x_misclassified_mnist.py
@@ -1,21 +1,18 @@
-# coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
-import numpy as np
-import matplotlib.pyplot as plt
+from datasets.mnist import load_mnist
 from deep_convnet import DeepConvNet
-from dataset.mnist import load_mnist
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
 
 network = DeepConvNet()
-network.load_params("deep_convnet_params.pkl")
+network.load_params('deep_convnet_params.pkl')
 
-print("calculating test accuracy ... ")
-#sampled = 1000
-#x_test = x_test[:sampled]
-#t_test = t_test[:sampled]
+print('calculating test accuracy ... ')
+
+# sampled = 1000
+# x_test = x_test[:sampled]
+# t_test = t_test[:sampled]
 
 classified_ids = []
 
@@ -23,19 +20,19 @@ acc = 0.0
 batch_size = 100
 
 for i in range(int(x_test.shape[0] / batch_size)):
-    tx = x_test[i*batch_size:(i+1)*batch_size]
-    tt = t_test[i*batch_size:(i+1)*batch_size]
+    tx = x_test[i * batch_size:(i + 1) * batch_size]
+    tt = t_test[i * batch_size:(i + 1) * batch_size]
     y = network.predict(tx, train_flg=False)
     y = np.argmax(y, axis=1)
     classified_ids.append(y)
     acc += np.sum(y == tt)
-    
+
 acc = acc / x_test.shape[0]
-print("test accuracy:" + str(acc))
+print('test accuracy:' + str(acc))
 
 classified_ids = np.array(classified_ids)
 classified_ids = classified_ids.flatten()
- 
+
 max_view = 20
 current_view = 1
 
@@ -46,15 +43,16 @@ mis_pairs = {}
 for i, val in enumerate(classified_ids == t_test):
     if not val:
         ax = fig.add_subplot(4, 5, current_view, xticks=[], yticks=[])
-        ax.imshow(x_test[i].reshape(28, 28), cmap=plt.cm.gray_r, interpolation='nearest')
+        ax.imshow(x_test[i].reshape(28, 28),
+                  cmap=plt.cm.gray_r, interpolation='nearest')
         mis_pairs[current_view] = (t_test[i], classified_ids[i])
-            
+
         current_view += 1
         if current_view > max_view:
             break
 
-print("======= misclassified result =======")
-print("{view index: (label, inference), ...}")
+print('======= misclassified result =======')
+print('{view index: (label, inference), ...}')
 print(mis_pairs)
 
 plt.show()
