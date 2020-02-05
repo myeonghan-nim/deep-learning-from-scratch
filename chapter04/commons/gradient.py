11d7a81a3f5a36b26b80efbafb343ff40d028485
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-02-05 22:49:33 +0900

    add function of chapter04 file

--- chapter04/commons/gradient.py
+++ chapter04/commons/gradient.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+
 def _numerical_gradient_1d(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
